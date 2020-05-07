a318a9e446ca591f001a1d9f4ede069008b2e968
Dan H <16111574+danh955@users.noreply.github.com>
2020-05-07 15:37:45 +0900

    Started LiteDbLoader

---
+++ LiteDbLoader/LiteDbLoader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LiteDbLoader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
